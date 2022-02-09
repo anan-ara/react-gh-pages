596533b2798bdfaf3ab3011c3c4d17d41eb9ba68
Anan A <anan.ara2001@gmail.com>
2022-02-09 15:40:23 -0800

    Finish in class assignment

--- src/index.js
+++ src/index.js
@@ -4,9 +4,47 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// const data = {
+//     id: 512,
+//      name: "Hi",
+//      email: "rhodes@hmc.edu",
+//      phone: "(909) 555-1212"
+//    }
+const data = [
+  {
+    id: 512,
+     name: "Neil Rhodes",
+     email: "rhodes@hmc.edu",
+     phone: "(909) 555-1212"
+   },
+  {
+    id: 787,
+     name: "Barack Obama",
+     email: "ex-prez@whitehouse.gov",
+     phone: "(312) 555-1212"
+   }
+];
+
+const jsx_data = data.map(e => 
+    <div key={e.id} className='data'>
+      <span className='name'>
+        {e.name}
+      </span>
+      <span className='email'>
+        {e.email}
+      </span>
+      <span>
+        {e.phone}
+      </span>
+    </div>
+)
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <h1>
+      People
+    </h1>
+    {jsx_data}
   </React.StrictMode>,
   document.getElementById('root')
 );
